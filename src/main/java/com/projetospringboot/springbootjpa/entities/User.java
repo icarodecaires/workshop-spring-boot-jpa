211979449b554b20b3943e0d4ae630cd6995fa8d
Icaro Caires <icaro_de_caires@outlook.com>
2021-03-10 21:22:58 -0400

    Preparando Ambiente de banco de teste H2

--- src/main/java/com/projetospringboot/springbootjpa/entities/User.java
+++ src/main/java/com/projetospringboot/springbootjpa/entities/User.java
@@ -2,9 +2,17 @@ package com.projetospringboot.springbootjpa.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Define que o atribudo no banco ter auto incremento
 	private Long id;
 	private String name;
 	private String email;
