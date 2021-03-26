1f461681c35f03283aea4b0fb9a03a9c4e107dea
Icaro Caires <icaro_de_caires@outlook.com>
2021-03-25 21:02:44 -0400

    Insert, Delete, Update User e tratamento de algumas exceções

---
+++ src/main/java/com/projetospringboot/springbootjpa/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.projetospringboot.springbootjpa.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException (Object id) {
+		super("Resource not found. Id"+id);
+	}
+	
+}
